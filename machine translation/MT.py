fbc82603d04fc9f79143ef06be439b6c5259d714
lin wx <linwx.mail@gmail.com>
2019-11-19 01:07:14 +0800

    2

--- machine translation/MT.py
+++ machine translation/MT.py
@@ -1,7 +1,18 @@
-import spacy
-import torchtext
-from torchtext.data import Field, BucketIterator, TabularDataset
-en = spacy.load('en_core_web_sm')
+from torchtext.data import Field, BucketIterator
+from torchtext.datasets import TranslationDataset
 
-def tokenize_en(sentence):
-    return [tok.text for tok in en.tokenizer(sentence)]
+Lang1 = Field(init_token='<sos>', eos_token='<eos>')
+Lang2 = Field(init_token='<sos>', eos_token='<eos>')
+
+train = TranslationDataset(path='../Datasets/MT_data/',
+                           exts=('eng-fra.train.en', 'eng-fra.train.fr'),
+                        fields=[('Lang1', Lang1),('Lang2', Lang2)])
+
+train_iter, val_iter, test_iter = BucketIterator.splits((train, train, train),
+                                                            batch_size=1, repeat=False)
+Lang1.build_vocab(train)
+Lang2.build_vocab(train)
+
+for i, train_batch in enumerate(train_iter):
+    print('Lang1  : \n', [Lang1.vocab.itos[x] for x in train_batch.Lang1.data])
+    print('Lang2 : \n', [Lang2.vocab.itos[x] for x in train_batch.Lang2.data])
