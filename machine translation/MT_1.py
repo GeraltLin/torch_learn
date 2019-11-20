1943382d2052d76a6b05320819d3d2d39886dbc5
lin wx <linwx.mail@gmail.com>
2019-11-21 01:07:18 +0800

    2

--- machine translation/MT_1.py
+++ machine translation/MT_1.py
@@ -33,8 +33,7 @@ class Encoder(nn.Module):
         self.rnn = nn.GRU(input_size=embed_size,
                           hidden_size=num_hiddens,
                           num_layers=num_layers,
-                          dropout=drop_prob,
-                          batch_first=True)
+                          dropout=drop_prob)
 
     def forward(self, inputs, state):
         embedding = self.embedding(inputs)
@@ -58,20 +57,21 @@ def attention_model(input_size, attention_size):
 
 def attention_forward(model, enc_states, dec_state):
     # 将解码器隐藏状态广播到和编码器隐藏状态形状相同后进⾏行行连结
-    # print(dec_state.size())
-    # print(enc_states.size())
-    dec_states = dec_state.unsqueeze(dim=1).expand_as(enc_states)
+    dec_states = dec_state.unsqueeze(dim=0).expand_as(enc_states)
 
     enc_and_dec_states = torch.cat((enc_states, dec_states), dim=2)
+
     # 10 4 16
-    e = model(enc_and_dec_states)  # 形状为(批量量⼤⼩,时间步数, 1)
-    alpha = F.softmax(e, dim=1)  # 在时间步维度做softmax运算
+
+    e = model(enc_and_dec_states)  # 形状为(时间步数, 批量量⼤大⼩小, 1)
+    alpha = F.softmax(e, dim=0)  # 在时间步维度做softmax运算
     # print(alpha.size())
     # print(enc_states.size())
     # print((alpha * enc_states).size())
     # print((alpha * enc_states).sum(dim=0).size())
-    # print('hi',(alpha * enc_states).sum(dim=1).size())
-    return (alpha * enc_states).sum(dim=1)  # 返回背景变量量
+    # print('hi',(alpha * enc_states).sum(dim=0).size())
+
+    return (alpha * enc_states).sum(dim=0)  # 返回背景变量量
 
 
 class Decoder(nn.Module):
@@ -85,7 +85,7 @@ class Decoder(nn.Module):
                                          attention_size).to(device)
         # GRU的输⼊入包含attention输出的c和实际输⼊入, 所以尺寸是2 * embed_size
         self.rnn = nn.GRU(2 * embed_size, num_hiddens, num_layers,
-                          dropout=drop_prob,batch_first=True)
+                          dropout=drop_prob)
         self.out = nn.Linear(num_hiddens, vocab_size)
 
     def forward(self, cur_input, state, enc_states):
@@ -96,15 +96,13 @@ class Decoder(nn.Module):
         # 使⽤用注意⼒力力机制计算背景向量量
         c = attention_forward(self.attention, enc_states,
                               state[-1])
-        # print('c',c.size()) #256 64
         # 将嵌⼊入后的输⼊入和背景向量量在特征维连结
 
         input_and_c = torch.cat((self.embedding(cur_input), c), dim=1)  # (批量量⼤大⼩小, 2*embed_size)
-        # print('=',input_and_c.size())
         # 为输⼊入和背景向量量的连结增加时间步维，时间步个数为1
-        output, state = self.rnn(input_and_c.unsqueeze(1), state)
+        output, state = self.rnn(input_and_c.unsqueeze(0), state)
         # 移除时间步维，输出形状为(批量量⼤大⼩小, 输出词典⼤大⼩小)
-        output = self.out(output).squeeze(dim=1)
+        output = self.out(output).squeeze(dim=0)
         return output, state
 
     def begin_state(self, enc_state):
@@ -113,24 +111,19 @@ class Decoder(nn.Module):
 
 
 def batch_loss(encoder, decoder, X, Y, loss):
-    batch_size = X.shape[0]
+    batch_size = X.shape[1]
     enc_state = encoder.begin_state()
     enc_outputs, enc_state = encoder(X, enc_state)
-    # 初始化解码器的隐藏状态
+    # 初始化解码器器的隐藏状态
     dec_state = decoder.begin_state(enc_state).to(device)
     # 解码器器在最初时间步的输⼊入是BOS
     dec_input = torch.tensor([Lang2.vocab.stoi['<sos>']] * batch_size).to(device)
     # 我们将使⽤用掩码变量量mask来忽略略掉标签为填充项PAD的损失
     mask, num_not_pad_tokens = torch.ones(batch_size, ).to(device), 0
     l = torch.tensor([0.0]).to(device)
+    for y in Y:  # Y shape: (batch, seq_len)
 
-    for y in Y.permute(1,0):# Y shape: (batch, seq_len)
-        # Y (batch, seq_len) -> (seq_len, batch)->y(batch)
-        print('dec_input',dec_input.size())
-        print('dec_state',dec_state.size())
 
-        # dec_input : [256]
-        # dec_state : [2, 256, 64]
         dec_output, dec_state = decoder(dec_input, dec_state,
                                         enc_outputs)
         l = l + (mask * loss(dec_output, y)).sum()
@@ -149,9 +142,8 @@ def train(encoder, decoder, lr, num_epochs):
     for epoch in range(num_epochs):
         l_sum = 0.0
         for i, train_batch in enumerate(train_iter):
-            X = train_batch.Lang1.data.transpose(0, 1).to(device)
-
-            Y = train_batch.Lang2.data.transpose(0, 1).to(device)
+            X = train_batch.Lang1.data.to(device)
+            Y = train_batch.Lang2.data.to(device)
             enc_optimizer.zero_grad()
             dec_optimizer.zero_grad()
             encoder = encoder.to(device)
